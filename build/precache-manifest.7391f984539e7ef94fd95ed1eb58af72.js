d1ad086da0b2c1d2606d78ede5a6dd64cb53d9a0
Prem <premsahni0501@gmail.com>
2019-08-02 19:13:49 +0530

    build

---
+++ build/precache-manifest.7391f984539e7ef94fd95ed1eb58af72.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e9fa744129686d9731f87cb9ed8ca1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5942c950ee7d2c010a09",
+    "url": "/static/css/2.f17165b1.chunk.css"
+  },
+  {
+    "revision": "42184086270b9a3327be",
+    "url": "/static/css/main.7a834fa9.chunk.css"
+  },
+  {
+    "revision": "5942c950ee7d2c010a09",
+    "url": "/static/js/2.98c59503.chunk.js"
+  },
+  {
+    "revision": "42184086270b9a3327be",
+    "url": "/static/js/main.c1adb52a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "78971d67d5b121a4ab1ad13f0ffc1d71",
+    "url": "/static/media/athletics.78971d67.svg"
+  },
+  {
+    "revision": "5bef84ea58f870199ab3ff2371d58a82",
+    "url": "/static/media/basketball.5bef84ea.jpg"
+  },
+  {
+    "revision": "54c6e5b5b6daed0cedbdef12a99a64c5",
+    "url": "/static/media/cricket.54c6e5b5.svg"
+  },
+  {
+    "revision": "f3fca2bf8d4f3ac0c3328958cfb6a424",
+    "url": "/static/media/hockey.f3fca2bf.svg"
+  },
+  {
+    "revision": "291a071dfe7e565e44f40c8d10bb1bdd",
+    "url": "/static/media/kabaddi.291a071d.svg"
+  },
+  {
+    "revision": "6cb70b1fd835a08582f8293f6a59b679",
+    "url": "/static/media/logo.6cb70b1f.svg"
+  },
+  {
+    "revision": "3a2c2ae3193115997117cc38c801b15e",
+    "url": "/static/media/rugby.3a2c2ae3.svg"
+  },
+  {
+    "revision": "d2562f8d023a03bc3c5c883733bbe910",
+    "url": "/static/media/soccer.d2562f8d.svg"
+  },
+  {
+    "revision": "f7349b04b33c682557b582177dbd07ee",
+    "url": "/static/media/tennis.f7349b04.jpg"
+  }
+]);
