641f7bcc2cf68a5fe773c3af901724f2287f12bb
Prem <premsahni0501@gmail.com>
2019-08-05 12:34:26 +0530

    Updates

---
+++ precache-manifest.2da498a53878060cdf2046068f16bdb2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e125d0e664eb969875a53608b477cf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f889ebae906c195a03a0",
+    "url": "/static/css/6.f17165b1.chunk.css"
+  },
+  {
+    "revision": "42407c58d66e8f605b3d",
+    "url": "/static/css/main.89e9c108.chunk.css"
+  },
+  {
+    "revision": "f889ebae906c195a03a0",
+    "url": "/static/js/6.a7412991.chunk.js"
+  },
+  {
+    "revision": "eaec6eae20fe3d1ac6fc",
+    "url": "/static/js/7.5e7ef1ca.chunk.js"
+  },
+  {
+    "revision": "d094dc423830e0f07c7f",
+    "url": "/static/js/GameType.5c5d7c93.chunk.js"
+  },
+  {
+    "revision": "51bd569be794968a4142",
+    "url": "/static/js/MapWrapper.a0c7ce29.chunk.js"
+  },
+  {
+    "revision": "e394f7877d10c34a5679",
+    "url": "/static/js/Payment.16b30a77.chunk.js"
+  },
+  {
+    "revision": "b8115ac755cf644a68a1",
+    "url": "/static/js/YourTeam.c2f2ae0e.chunk.js"
+  },
+  {
+    "revision": "42407c58d66e8f605b3d",
+    "url": "/static/js/main.0735ae4d.chunk.js"
+  },
+  {
+    "revision": "1eed313c5ac5fd41d2e3",
+    "url": "/static/js/runtime~main.a9043e7d.js"
+  },
+  {
+    "revision": "78971d67d5b121a4ab1ad13f0ffc1d71",
+    "url": "/static/media/athletics.78971d67.svg"
+  },
+  {
+    "revision": "5bef84ea58f870199ab3ff2371d58a82",
+    "url": "/static/media/basketball.5bef84ea.jpg"
+  },
+  {
+    "revision": "54c6e5b5b6daed0cedbdef12a99a64c5",
+    "url": "/static/media/cricket.54c6e5b5.svg"
+  },
+  {
+    "revision": "f3fca2bf8d4f3ac0c3328958cfb6a424",
+    "url": "/static/media/hockey.f3fca2bf.svg"
+  },
+  {
+    "revision": "291a071dfe7e565e44f40c8d10bb1bdd",
+    "url": "/static/media/kabaddi.291a071d.svg"
+  },
+  {
+    "revision": "6cb70b1fd835a08582f8293f6a59b679",
+    "url": "/static/media/logo.6cb70b1f.svg"
+  },
+  {
+    "revision": "3a2c2ae3193115997117cc38c801b15e",
+    "url": "/static/media/rugby.3a2c2ae3.svg"
+  },
+  {
+    "revision": "d2562f8d023a03bc3c5c883733bbe910",
+    "url": "/static/media/soccer.d2562f8d.svg"
+  },
+  {
+    "revision": "f7349b04b33c682557b582177dbd07ee",
+    "url": "/static/media/tennis.f7349b04.jpg"
+  }
+]);
